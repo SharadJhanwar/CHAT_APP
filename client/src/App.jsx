1cce308e9b643e7d8a41982ca6a1c78bbbf1d6fb
SharadJhanwar <sharadjhawar12345@gmail.com>
2025-09-07 23:17:47 +0530

    add dummy frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,12 +7,7 @@ import ProfilePage from './pages/ProfilePage'
 const App = () => {
   return (
     <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
-      <nav>
-        <Link to="/">Home</Link>
-        <Link to="/login" style={{ marginLeft: '10px' }}>Login</Link>
-        <Link to="/profile" style={{ marginLeft: '10px' }}>Profile</Link>
-      </nav>
-
+      
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
--- client/src/pages/ProfilePage.jsx
+++ client/src/pages/ProfilePage.jsx
@@ -1,9 +1,61 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import assets from '../assets/assets';
 
 const ProfilePage = () => {
+  const [selectedImg, setSelectedImg] = useState(null);
+  const navigate = useNavigate();
+  const [name, setName] = useState("Martin Johnson");
+  const [bio, setBio] = useState("Hi everyone, i am using quick chat");
+
+  const  handleSubmit = async (e) => {
+    e.preventDefault();
+    navigate('/')
+  }
+
   return (
-    <div>
-      <h1>ProfilePage</h1>
+    <div className='min-h-screen bg-cover bg-no-repeat flex items-center justify-center'>
+      <div className='w-5/6 max-w-2xl backdrop-blur-2xl text-gray-300 border-2 border-gray-600 flex items-center justify-between max-sm:flex-col-reverse rounded-lg'>
+        <form onSubmit={handleSubmit} className='flex flex-col gap-5 p-10 flex-1'>
+          <h3 className='text-lg '>Profile Details</h3>
+          <label htmlFor="avatar" className='flex items-center gap-3 cursor-pointer'>
+
+            {/*upload image*/}
+            <input
+              onChange={(e) => {
+                setSelectedImg(e.target.files[0])
+              }}
+              type="file" name="" id="avatar" accept='.png .jpg .jpeg' hidden />
+            <img src={selectedImg ? URL.createObjectURL(selectedImg) : assets.avatar_icon} alt="" className={`w-12 h-12 ${selectedImg && 'rounded-full'}`} />
+            Upload Profile Image
+          </label>
+
+          {/*name*/}
+          <input
+            onChange={(e) => { setName(e.target.value) }}
+            value={name}
+            type="text" required placeholder='your name'
+            className='p-2  border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500'
+          />
+
+
+          {/*Bio*/}
+          <textarea
+            onChange={() => {
+              setBio(e.target.value)
+            }}
+            value={bio}
+            rows={4}
+            className='p-2 border border-gray-500 rounded-md focus:outline-none focus:ring-2 focus:ring-violet-500'
+            placeholder='Write profile bio'
+            required
+          ></textarea>
+
+          {/*Save button*/}
+          <button type='submit' className='bg-gradient-to-r from-purple-400 to-violet-600 text-white p-2 rounded-full text-lg cursor-pointer'>Save</button>
+        </form>
+        <img src={assets.logo_icon} className='max-w-40 aspect-sqaure rounded-full mx-10 max-sm:mt-10' alt="" />
+      </div>
     </div>
   )
 }
